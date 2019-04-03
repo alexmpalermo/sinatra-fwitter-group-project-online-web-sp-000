af6aa11ba6ed7bcc5d9ad1d59be0a8bab67802e9
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-03 16:29:58 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,10 @@ class UsersController < ApplicationController
   
   get '/signup' do 
     if !logged_in?
+      
       redirect to '/tweets'
     else
-    erb :'/users/create_user'
+    
   end 
   end
   
