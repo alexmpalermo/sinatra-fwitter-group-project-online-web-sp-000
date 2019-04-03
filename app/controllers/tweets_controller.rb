eaf8e0db3bd15a16c0264c8d0c0718896106381e
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-03 19:03:59 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -30,8 +30,9 @@ end
   post '/tweets' do 
     if params[:content] != ""
     @tweet = Tweet.create(content: params[:content])
-    @tweet.user = current_user
-    @tweet.save
+    @user = current_user
+    @tweet.user = @user
+    @user.save
     redirect to "/tweets/#{@tweet.id}"
   else 
     redirect to "/tweets/new"
