fb3ae5b9b2a31364c0649ba379b5c22d94a84105
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-03 16:08:04 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,40 @@
 class TweetsController < ApplicationController
 
-
+  get '/tweets' do 
+    @tweets = Tweet.all
+    erb :'/tweets/index'
+  end 
+  
+  get '/tweets/new' do 
+    erb :'/tweets/new'
+  end 
+  
+  get '/tweets/:id' do 
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'/tweets/show'
+  end
+  
+  post '/tweets' do 
+    @tweet = Tweet.create(params[:tweet])
+    
+    redirect to "/tweets/#{@tweet.id}"
+  end
+  
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'/tweets/edit'
+  end
+  
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.update(params[:tweet])
+    redirect to "/tweets/#{@tweet.id}"
+  end
+  
+  delete '/tweets/:id/delete' do 
+     
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.clear
+    redirect to "/tweets"
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,56 @@
 class UsersController < ApplicationController
-
-
+  
+  get '/signup' do 
+    if logged_in?
+      redirect to "/tweets"
+    else
+    erb :'/users/create_user'
+  end 
+  end
+  
+  get '/login' do 
+     if logged_in?
+      redirect to "/tweets"
+    else
+    erb :'/users/login'
+  end
+end 
+  
+  get '/user/:slug' do
+     if logged_in?
+    @user = current_user
+    erb :'/users/show'
+  else
+    redirect to "/login"
+  end
+  end
+  
+  post '/signup' do
+    if params[:username] != "" &&  params[:password] != "" && params[:email] != ""
+    @user = User.create(username: params[:username], password: params[:password], email: params[:email])
+    session[:slug] = @user.slug
+    redirect to "/tweets"
+  else 
+    redirect to "/signup"
+  end
+  end
+  
+  post '/login' do 
+    if params[:username] != "" &&  params[:password] != "" 
+      @user = User.find_by(:username => params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:slug] = @user.slug
+        redirect to "/tweets"
+      else
+        redirect '/login'
+      end
+    else
+      redirect '/login'
+    end
+  end
+  
+  get '/logout' do
+    session.clear
+    redirect to "/users/login"
+  end
 end
